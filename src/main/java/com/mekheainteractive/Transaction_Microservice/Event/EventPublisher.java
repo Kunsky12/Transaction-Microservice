3fbaac56b85e70c5bd163d1e9f233b520585d274
Kun Sky <77968149+Kunsky12@users.noreply.github.com>
2026-03-29 10:38:53 +0700

    Delete src/main/java/com/mekheainteractive/Transaction_Microservice/Event/EventPublisher.java

--- src/main/java/com/mekheainteractive/Transaction_Microservice/Event/EventPublisher.java
+++
@@ -1,8 +0,0 @@
-package com.mekheainteractive.Transaction_Microservice.Event;
-
-import com.mekheainteractive.Transaction_Microservice.Entity.TransactionEntity;
-
-public interface EventPublisher {
-
-    void publishTransactionNotification(TransactionEntity tx); // new method for WS
-}
